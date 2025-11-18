f1cd62a6729d0d14b2ca5ce9d8864997aebf1417
PUAAT <s1131412@o365st.pu.edu.tw>
2025-11-18 16:30:32 +0800

    Update mian.js

--- Js/mian.js
+++ Js/mian.js
@@ -3,21 +3,21 @@
  * 我們的目標：當頁面向下捲動時，幫 <nav> 加上 "scrolled" 這個 class
  */
 
-// 1. 選中我們要操作的目標 (導覽列)
+// 1. 抓取我們的導覽列 (nav) 標籤
 const nav = document.querySelector('nav');
 
-// 2. 監聽整個視窗的「捲動 (scroll)」事件
+// 2. 監聽視窗的「捲動 (scroll)」事件
 window.addEventListener('scroll', () => {
     
-    // 3. 檢查：如果垂直捲動的距離 (window.scrollY)「大於」 50 像素
+    // 3. 檢查：如果目前捲動的垂直距離 (scrollY) 大於 50 像素...
     if (window.scrollY > 50) {
         
-        // 4. 就幫 <nav> 加上 "scrolled" 這個 class
+        // ...就幫 nav 加上一個叫做 'scrolled' 的 class
         nav.classList.add('scrolled');
         
     } else {
         
-        // 5. 否則 (捲回頂部了)，就把 "scrolled" 這個 class 移除
+        // 4. 如果捲回頂部 (小於 50 像素)，就把 'scrolled' 拿掉
         nav.classList.remove('scrolled');
     }
 });
